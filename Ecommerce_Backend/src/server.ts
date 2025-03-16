2ee950934eab54635a900e3b1bdaab2301a757db
tronghghe172557 <hoanggiatrang01@gmail.com>
2025-03-17 05:52:37 +0700

    refactor: combine code segments in one file into a function

--- Ecommerce_Backend/src/server.ts
+++ Ecommerce_Backend/src/server.ts
@@ -8,34 +8,38 @@ import { redis } from '~/base/redis'
 import { envVariables, Logger } from '~/base/common/utils'
 import { HttpExceptionHandler } from '~/base/common/handlers'
 
-const app = express()
-const logger = new Logger('Server')
-// Middleware bảo mật HTTP headers
-app.use(helmet())
+const bootstrap = async () => {
+  const app = express()
+  const logger = new Logger('Server')
+  // Middleware bảo mật HTTP headers
+  app.use(helmet())
 
-// Middleware ghi log request
-app.use(morgan('dev'))
+  // Middleware ghi log request
+  app.use(morgan('dev'))
 
-// Middleware nén dữ liệu response
-app.use(compression())
+  // Middleware nén dữ liệu response
+  app.use(compression())
 
-//
-app.use(express.json()) // Middleware này giúp Express parse JSON từ request body.
-app.use(express.urlencoded({ extended: true })) // Middleware này giúp Express parse URL encoded data từ request body.
+  //
+  app.use(express.json()) // Middleware này giúp Express parse JSON từ request body.
+  app.use(express.urlencoded({ extended: true })) // Middleware này giúp Express parse URL encoded data từ request body.
 
-// connect to MongoDB
-Database.getInstance()
-// connect to Redis
-redis.connect()
+  // connect to MongoDB
+  Database.getInstance()
+  // connect to Redis
+  redis.connect()
 
-// Route cơ bản
-app.use('/v1/api', appRouter)
+  // Route cơ bản
+  app.use('/v1/api', appRouter)
 
-// func handle error
-app.use(HttpExceptionHandler)
+  // func handle error
+  app.use(HttpExceptionHandler)
 
-// Chạy server
-const PORT = envVariables.PORT || 3000
-app.listen(PORT, () => {
-  logger.info(`Server is running on http://localhost:${PORT}`)
-})
+  // Chạy server
+  const PORT = envVariables.PORT || 3000
+  app.listen(PORT, () => {
+    logger.info(`Server is running on http://localhost:${PORT}`)
+  })
+}
+
+bootstrap()
